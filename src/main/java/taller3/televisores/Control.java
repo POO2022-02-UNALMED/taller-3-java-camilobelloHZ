bdfc47b50b58ee7e7618d4f9e7b0b796a380621f
camilobelloHZ <cbelloh@unal.edu.co>
2022-08-29 23:01:57 -0500

    taller 3 javax

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,48 +1,47 @@
 package taller3.televisores;
 
 public class Control {
+	private TV tv;
 	
-    private TV tv;
-    public int canal1;
-
-    public TV getTv() {
-        return tv;
-    }
-
-    public void setTv(TV tv) {
-        this.tv = tv;
-    }
-
-    public void enlazar(TV tv){
-        this.tv = tv;
-        tv.setControl(this);
-    }
-
-    public void canalUp(){
-        tv.canalUp();
-    }
-
-    public void canalDown(){
-        tv.canalDown();
-    }
-
-    public void volumenUp(){
-        tv.volumenUp();
-    }
-
-    public void volumenDown(){
-        tv.volumenDown();
-    }
-
-    public void setCanal(int canal1){
-        tv.setCanal(canal1);
-    }
-
-    public void turnOn() {
-        tv.turnOn();
-    }
-    
-    public void turnOff() {
-        tv.turnOff();
-    }
+	public void turnOn(){
+		tv.turnOn();
+	}
+	
+	public void turnOff(){
+		tv.turnOff();
+	}
+	
+	public void canalUp(){
+		tv.canalUp();
+	}
+	
+	public void canalDown(){
+		tv.canalDown();	
+	}
+	
+	public void volumenUp(){
+		tv.volumenUp();
+	}
+	
+	public void volumenDown(){
+		tv.volumenDown();
+	}
+	
+	public void setCanal(int canal) {
+		tv.setCanal(canal);
+	}
+	
+	public void enlazar(TV tv) {
+		this.tv=tv;
+		tv.setControl(this);
+	}
+	
+	public void setTv(TV tv) {
+		this.tv=tv;
+	}
+	
+	public TV getTv() {
+		return tv;
+	}
+	
 }
--- src/main/java/taller3/televisores/Marca.java
+++ src/main/java/taller3/televisores/Marca.java
@@ -1,26 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package taller3.televisores;
 
-/**
- *
- * @author Roger Vera
- */
 public class Marca {
-    private String nombre;
-
-    public Marca(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    } 
-    
+	private String nombre;
+	
+	public Marca(String nombre) {// constructor
+		this.nombre=nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre=nombre;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
 }
